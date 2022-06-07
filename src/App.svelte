d7c82d55d34898257ebaa61b4b3fcedceec369b7
LukaOber <63709279+LukaOber@users.noreply.github.com>
2022-06-07 09:16:17 +0200

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+	export let name: string;
+</script>
+
+<main class="max-w-xl p-4 mx-auto text-center">
+	<h1 class="text-6xl font-thin leading-normal text-blue-600 uppercase">Hello {name}!</h1>
+	<p class="custom-style mt-[3rem]">
+		Visit the
+		<a href="https://svelte.dev/tutorial" class="text-blue-500 underline">Svelte tutorial</a>
+		to learn how to build Svelte apps.
+	</p>
+</main>
